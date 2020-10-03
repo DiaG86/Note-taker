5c6cb5cf8f6e2d2e48c597109e7d31668a95789f
DiaG86 <adia.grigsby@gmail.com>
2020-10-03 19:29:08 -0400

    created server file and files for th routes

---
+++ Develop/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes')
+const apiRoutes = require('./routes/API_Route')
+
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"))
+
+app.use("/",htmlRoutes);
+app.use("/api",apiRoutes);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
