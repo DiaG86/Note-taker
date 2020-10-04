68b4ed773cf579829448831a98a22d995926bdf3
DiaG86 <adia.grigsby@gmail.com>
2020-10-04 00:07:50 -0400

    update

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -26,6 +26,7 @@ const saveNote = (note) => {
 
 // A function for deleting a note from the db
 const deleteNote = (id) => {
+  console.log("working")
   return $.ajax({
     url: "api/notes/" + id,
     method: "DELETE",
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,18 +1,17 @@
-const {v4:uuidv4} = require("uuid");
+const {v4: uuidv4} = require("uuid");
 const dbFile = require("../db/db");
 const fs = require("fs");
 
-
-
 module.exports=function(app){
-    let noteID =uuidv4()
 
-console.log("Note ID: "+noteID);
+let noteId = uuidv4();
+
+console.log("Note ID: "+noteId);
 
 app.post("/api/notes", function(req, res) {
     console.log(req.body)
-    let newEntry = {title: req.body.title, text: req.body.text, id: uuidv4()};
-    dbFile.push(newEntry);
+    let newNote = {title: req.body.title, text: req.body.text, id: uuidv4()};
+    dbFile.push(newNote);
     fs.writeFileSync("db/db.json",JSON.stringify(dbFile),"utf-8");
       res.json(dbFile);
 });
@@ -22,12 +21,11 @@ app.get("/api/notes",function(req,res){
     var temp = JSON.parse(noteData);
     res.json(temp);
 });
-    
-      
+
 app.delete("/api/notes/:id", (req, res) => {
-    console.log("anything");
-    var notes = fs.readFileSync("db/db.json","utf-8");
-    var temp = JSON.parse(notes);
+    console.log("WORK!");
+    var noteData = fs.readFileSync("db/db.json","utf-8");
+    var temp = JSON.parse(noteData);
     let newData=temp.filter((note) =>{ return note.id !==req.params.id})    
     fs.writeFileSync("db/db.json", JSON.stringify(newData));
 
@@ -35,3 +33,4 @@ app.delete("/api/notes/:id", (req, res) => {
   });
 
 }
+
